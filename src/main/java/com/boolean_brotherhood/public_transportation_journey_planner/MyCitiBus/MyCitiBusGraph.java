148ad530116889a1dbbc0b58147071c21b68ec47
Theletsane <theletsanei47@gmail.com>
2025-09-27 01:15:28 +0200

    try

--- src/main/java/com/boolean_brotherhood/public_transportation_journey_planner/MyCitiBus/MyCitiBusGraph.java
+++ src/main/java/com/boolean_brotherhood/public_transportation_journey_planner/MyCitiBus/MyCitiBusGraph.java
@@ -123,8 +123,8 @@ public final class MyCitiBusGraph{
                 String[] parts = line.split(",", -1);
                 String routeCode = parts[0];
                 String routeFullName = parts[1];
-                Path dir1RoutePath = Paths.get("main/resources/CapeTownTransitData/MyCiti_Data/myciti-bus-schedules/" + routeCode + "-dir1.csv");
-                Path dir2RoutePath = Paths.get("main/resources/CapeTownTransitData/MyCiti_Data/myciti-bus-schedules/" + routeCode + "-dir2.csv");
+                Path dir1RoutePath = Paths.get("CapeTownTransitData/MyCiti_Data/myciti-bus-schedules/" + routeCode + "-dir1.csv");
+                Path dir2RoutePath = Paths.get("resources/CapeTownTransitData/MyCiti_Data/myciti-bus-schedules/" + routeCode + "-dir2.csv");
 
                 getTripsInRoute(dir1RoutePath, routeFullName);
                 getTripsInRoute(dir2RoutePath, routeFullName);
