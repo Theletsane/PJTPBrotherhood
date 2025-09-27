d7b7352303532ead917a0fa90583e46e76ed747e
Theletsane <theletsanei47@gmail.com>
2025-09-27 03:01:35 +0200

    try

--- src/main/java/com/boolean_brotherhood/public_transportation_journey_planner/MyCitiBus/MyCitiBusGraph.java
+++ src/main/java/com/boolean_brotherhood/public_transportation_journey_planner/MyCitiBus/MyCitiBusGraph.java
@@ -127,7 +127,7 @@ public final class MyCitiBusGraph{
                 Path dir2RoutePath = Paths.get("CapeTownTransitData/MyCiti_Data/myciti-bus-schedules/" + routeCode + "-dir2.csv");
 
                 getTripsInRoute("CapeTownTransitData/myciti-bus-schedules/" + routeCode + "-dir1.csv", routeFullName);
-                getTripsInRoute("CapeTownTransitData/myciti-bus-schedules/" + routeCode + "-dir2.csv", routeFullName);
+                getTripsInRoute("CapeTownTransitData/MyCiti_Data/myciti-bus-schedules/" + routeCode + "-dir2.csv", routeFullName);
 
                 }
             }
